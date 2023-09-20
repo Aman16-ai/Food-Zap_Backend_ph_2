31967d84b536a90e451491927c0d6d5b926d7b61
aman <asaxena7531@gmail.com>
2023-09-20 13:25:59 +0530

    sms util auth token error fix

--- src/utils/smsUtil.js
+++ src/utils/smsUtil.js
@@ -7,21 +7,36 @@ const twilio = require("twilio");
 
 const client = twilio(
   "AC9c5572b1664f0442c08d307cbb3ab8be",
-  "070fbe2c8ec87d7feb9c1095d56e62db"
+  "3bd8cef308455c6f76f8d21c25448383"
 );
 
-const sendMessage = (number,message) => {
+const sendMessage = async(number,message) => {
   console.log("number : "+number," message : "+message)
   const senderNumber = "+13612648897";
-  client.messages
-    .create({
-      body: message,
-      from: senderNumber,
-      to: number,
+  // const result = client.messages
+  //   .create({
+  //     body: message,
+  //     from: senderNumber,
+  //     to: number,
+  //   })
+  //   .then((message) => console.log("Message Sent"))
+  //   .catch((err) => console.log(err));
+  try {
+    const result = await client.messages.create({
+      body:message,
+      from:senderNumber,
+      to:number
     })
-    .then((message) => console.log("Message Sent"))
-    .catch((err) => console.log(err));
+    console.log(result)
+    return result
+  }
+  catch(err) {
+    console.log("error agai",err)
+    return err
+  }
+
+    
 };
 
-// sendMessage("+918285866269","hi foodzap")
+sendMessage("+918285866269","hi foodzap")
 module.exports = sendMessage
